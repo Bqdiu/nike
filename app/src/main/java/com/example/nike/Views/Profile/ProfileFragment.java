48d26ca90f5d43261aa5768e36e868532f47074f
bqdiu <boquangdieu2003@gmail.com>
2024-05-03 13:20:08 +0700

    update login google
    use Credential Manager to login with Google

--- app/src/main/java/com/example/nike/Views/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/nike/Views/Profile/ProfileFragment.java
@@ -1,15 +1,26 @@
 package com.example.nike.Views.Profile;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.nike.R;
+import com.example.nike.Views.Register;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.zip.Inflater;
 
@@ -30,26 +41,62 @@ public class ProfileFragment extends Fragment {
     private String mParam2;
 
 
-    CardView cs_inbox;
+    private SharedPreferences sharedPreferences;
+    CardView cv_inbox;
+    TextView user_name;
+
+    //test logout
+    CardView cv_setting;
+
     public ProfileFragment() {
         // Required empty public constructor
     }
 
     private void addControls(View view)
     {
-        cs_inbox = view.findViewById(R.id.cs_inbox);
+        user_name = view.findViewById(R.id.user_name);
+        cv_inbox = view.findViewById(R.id.cs_inbox);
+        sharedPreferences = view.getContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        cv_setting = view.findViewById(R.id.cv_setting);
     }
 
     private void addEvents()
     {
-        cs_inbox.setOnClickListener(new View.OnClickListener() {
+        cv_inbox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ((ProfileFragmentInboxListener) getActivity()).loadInboxFragment();
             }
         });
+        cv_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut(v);
+            }
+        });
     }
 
+    private void loadDataUser()
+    {
+        String us = sharedPreferences.getString("user_name",null);
+        user_name.setText(us);
+    }
+
+    private void signOut(View view)
+    {
+        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(getActivity(), new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build());
+        googleSignInClient.signOut().addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                // Delete user info from SharePreferences
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                Intent intent = new Intent(view.getContext(), Register.class);
+                startActivity(intent);
+            }
+        });
+    }
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -82,6 +129,7 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile,container,false);
         addControls(view);
+        loadDataUser();
         addEvents();
         return view;
 
---
+++ app/src/main/java/com/example/nike/Views/Register.java
@@ -0,0 +1,169 @@
+package com.example.nike.Views;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.nike.MainActivity;
+import com.example.nike.R;
+import com.example.nike.Model.DBConnection;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+
+import java.sql.Connection;
+
+public class Register extends AppCompatActivity {
+    EditText usernameEditText, passwordEditText, emailEditText, numberPhoneEditText;
+    MaterialButton signUpButton;
+    ImageView btn_google;
+    private static final int REQUEST_CODE_SIGN_IN = 9001;
+    private GoogleSignInClient mGoogleSignInClient;
+    private SharedPreferences sharedPreferences;
+
+    private void addControls()
+    {
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        emailEditText = findViewById(R.id.email);
+        numberPhoneEditText = findViewById(R.id.number_phone);
+        signUpButton = findViewById(R.id.signupbtn);
+        btn_google = findViewById(R.id.btn_SignInGoogle);
+    }
+
+    private void addEvents()
+    {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy thông tin từ các trường EditText
+                String username = usernameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String numberPhone = numberPhoneEditText.getText().toString();
+
+                // Kiểm tra thông tin đăng ký hợp lệ
+                if (isValidInformation(username, email, password, numberPhone)) {
+                    // Thực hiện kết nối và đăng ký người dùng
+                    DBConnection dbConnection = new DBConnection();
+                    Connection conn = dbConnection.connectionClass();
+
+                    if (conn != null) {
+                        boolean isSuccess = dbConnection.addUser(username, password, "", email, numberPhone, "", "", "", 0, 0);
+
+                        if (isSuccess) {
+                            Toast.makeText(Register.this, "Sign up successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Register.this, "Sign up failed", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(Register.this, "Database connection failed", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(Register.this, "Invalid information", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        addControls();
+        loadGoogleSignIn();
+        addEvents();
+    }
+
+    private void loadGoogleSignIn()
+    {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // Khởi tạo SharedPreferences
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        // ...
+
+        // Kiểm tra xem người dùng đã đăng nhập chưa
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Kết quả trả về từ launch của Intent từ GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Đăng nhập thành công, lưu thông tin đăng nhập
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("email", account.getEmail());
+            editor.putString("user_name",account.getGivenName());
+            editor.apply();
+            updateUI(account);
+        } catch (ApiException e) {
+            // Mã lỗi ApiException chỉ ra trạng thái đăng nhập chi tiết.
+//            Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(this, "Dang nhap that bai", Toast.LENGTH_SHORT).show();
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account != null) {
+            // User logged
+            Intent intent = new Intent(Register.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            // User not log in or is log out
+        }
+    }
+
+    private boolean isValidInformation(String username, String email, String password, String numberPhone) {
+        return !username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !numberPhone.isEmpty();
+    }
+}
