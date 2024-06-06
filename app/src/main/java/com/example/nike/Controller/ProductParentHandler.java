a5c6fb658a5cf09f216d2a065d484480d4d9b2dd
bqdiu <boquangdieu2003@gmail.com>
2024-06-07 01:36:25 +0700

    test view home fragment

--- app/src/main/java/com/example/nike/Controller/ProductParentHandler.java
+++ app/src/main/java/com/example/nike/Controller/ProductParentHandler.java
@@ -65,6 +65,34 @@ public class ProductParentHandler {
         }
         return list;
     }
+
+    public static ArrayList<ProductParent> getAllNewRelease(){
+        ArrayList<ProductParent> list = new ArrayList<>();
+        Connection conn = dbConnection.connectionClass();
+        if(conn!=null){
+            String query = "select * from product_parent where is_new_release = 'true'";
+            try{
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()){
+                    ProductParent pp = new ProductParent();
+                    pp.setId(rs.getInt(1));
+                    pp.setName(rs.getString(2));
+                    pp.setObjectID(rs.getInt(3));
+                    pp.setCategoryID(rs.getInt(4));
+                    pp.setThumbnail(rs.getString(5));
+                    pp.setPrice(rs.getInt(6));
+                    pp.setNewRelease(rs.getBoolean(7));
+                    pp.setIconsID(rs.getInt(8));
+                    list.add(pp);
+                }
+            }catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+        return list;
+    }
     public static ArrayList<ProductParent> getProductParentByName(String name){
         ArrayList<ProductParent> list = new ArrayList<>();
         Connection conn = null;
--- app/src/main/java/com/example/nike/Views/Home/HomeFragment.java
+++ app/src/main/java/com/example/nike/Views/Home/HomeFragment.java
@@ -1,48 +1,51 @@
 package com.example.nike.Views.Home;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.nike.Controller.ProductParentHandler;
+import com.example.nike.Model.Product;
+import com.example.nike.Model.ProductParent;
 import com.example.nike.R;
+import com.example.nike.Views.Shop.Adapter.ItemRecycleViewAdapter;
+import com.example.nike.Views.Shop.Product.DetailProduct;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements ItemRecycleViewAdapter.ItemClickListener{
 
     TextView tvGreeting;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
+    private SharedPreferences sharedPreferences;
     private String mParam1;
     private String mParam2;
+    private RecyclerView rcv_newRelease;
+    private ArrayList<ProductParent> productParentList = new ArrayList<>();
+    private ArrayList<ProductParent> limitProductParentList = new ArrayList<>();
+    private ItemRecycleViewAdapter adapter;
+    private String user_name;
 
     public HomeFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
@@ -51,7 +54,6 @@ public class HomeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,21 +62,47 @@ public class HomeFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         addControl(view);
+        rcv_loadData();
         addEvent();
         return view;
     }
     private void addControl(View view){
         tvGreeting = view.findViewById(R.id.greeting);
-    }
-    private void addEvent(){
+        rcv_newRelease = view.findViewById(R.id.rcv_newRelease);
+        sharedPreferences = view.getContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        user_name = sharedPreferences.getString("first_name",null);
         tvGreeting.setText(checkHourGreeting());
+    }
+    private void rcv_loadData()
+    {
+        productParentList = ProductParentHandler.getAllNewRelease();
+        for(ProductParent productParent : productParentList)
+        {
+            if(limitProductParentList.size() < 3)
+               limitProductParentList.add(productParent);
+        }
+        adapter = new ItemRecycleViewAdapter(getContext(),limitProductParentList,this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(),RecyclerView.HORIZONTAL,false);
+        rcv_newRelease.setLayoutManager(layoutManager);
+        rcv_newRelease.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(ArrayList<Product> list) {
+        Fragment fragment = DetailProduct.newInstance(list);
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayout,fragment);
+        ft.addToBackStack("TabLayoutOfShop");
+        ft.commit();
+    }
+
+    private void addEvent(){
+
     }
     private String checkHourGreeting(){
         String msg="";
@@ -83,18 +111,16 @@ public class HomeFragment extends Fragment {
             LocalTime time = LocalTime.now();
             int hour = time.getHour();
             if(hour>=6 && hour<12){
-                msg = "Good Morning, Dung";
+                msg = "Good Morning, " + user_name;
             }
             else if(hour>=12 && hour<18){
-                msg = "Good Afternoon, Dung";
+                msg = "Good Afternoon, " + user_name;
             }
             else
             {
-                msg = "Good Evening, Dung";
+                msg = "Good Evening, " + user_name;
             }
         }
-
-
         return msg;
     }
 }
